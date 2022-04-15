56e2d2207a6f304a84760b1f35f1d0f295534b46
priyanka <priyanka.chavan1408@gmail.com>
2022-04-15 13:17:02 +0530

    [Priyanka]Add:Test case check the length of the dictionary.

---
+++ test_AddressBook.py
@@ -0,0 +1,15 @@
+import unittest
+from AddressBook import AddressBook
+book = AddressBook()
+
+
+class TestAddressBook(unittest.TestCase):
+    def test_addContacts(self):
+        book = AddressBook()
+        book.addContacts("Siya","Salunkhe","Satara","Satara","Maharastra","415507","7066647330","priyanka.chavan1408@gmail.com")
+        self.assertEqual(len(book.contact),1)
+        self.assertTrue(len(book.contact), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
