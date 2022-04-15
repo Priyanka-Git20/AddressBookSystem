5bd9107aab529031191aec522d9abcb46ad4f597
priyanka <priyanka.chavan1408@gmail.com>
2022-04-15 13:41:32 +0530

    [Priyanka]Add:Check the person is deleted or not by checking the length bof the directory.

--- test_AddressBook.py
+++ test_AddressBook.py
@@ -16,6 +16,10 @@ class TestAddressBook(unittest.TestCase):
                          "priyanka.chavan1408@gmail.com")
         self.assertEqual(book.contact[0][0], "piya")
 
+    def test_deleteContacts(self):
+        book.deleteContacts()
+        self.assertGreaterEqual(len(AddressBook.contact), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
